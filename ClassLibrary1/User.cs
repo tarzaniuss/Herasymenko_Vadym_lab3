02644e15e2bbc458f2d16d682092b13d2f816897
Vadym <gerasimenko.vadim1411@gmail.com>
2022-05-21 12:33:19 +0300

    aded library

---
+++ ClassLibrary1/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime Created { get; set; }
+
+        public static void ShowUser(User user)
+        {
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Email);
+        }
+    }
+}
